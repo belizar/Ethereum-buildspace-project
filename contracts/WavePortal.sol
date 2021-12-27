5604e9f8e42dd3a696e621765012e1eb8ec28f7f
Benjamin Lizarraga <belizar@Benjamins-MBP.fibertel.com.ar>
2021-12-27 01:40:36 -0300

    storing data

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -7,6 +7,7 @@ import "hardhat/console.sol";
 contract WavePortal {
     // it stored permanently in contract storage
     uint256 totalWaves;
+    mapping(address => uint) public waves;
 
     constructor() {
         console.log("Hey I'm a contract");
@@ -14,7 +15,14 @@ contract WavePortal {
 
     function wave() public {
         totalWaves += 1;
+        waves[msg.sender] += 1;
         console.log('%s has was waved;', msg.sender);
+    }
+
+    function unwave() public {
+        totalWaves -= 1;
+        waves[msg.sender] -= 1;
+        console.log('%s has was unwaved;', msg.sender);
     }
 
     function getTotalWaves() public view returns (uint256) {
